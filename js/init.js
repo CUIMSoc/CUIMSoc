300e26fc36f80eb3bde6df86d9b8f5a0afb2ef89
Akash Lakshmi Narasimha <purpledragon008@gmail.com>
2019-09-09 11:07:13 +0100

    images

--- js/init.js
+++ js/init.js
@@ -3,6 +3,12 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+      $(document).ready(function(){
+    $('.slider').slider();
+  });
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+
