1d74b39518d14926565aeb43401cdffe676d6914
Akash Lakshmi Narasimha <purpledragon008@gmail.com>
2019-09-09 11:37:51 +0100

    Update init.js

--- js/init.js
+++ js/init.js
@@ -5,12 +5,10 @@
     $('.parallax').parallax();
       $(document).ready(function(){
     $('.slider').slider({
-    	//height: 800,
-    	//duration: 2000
+    	height: 400,
+    	duration: 2000
     });
-
   });
-
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
